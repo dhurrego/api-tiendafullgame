45009a9f9ac760de5a91b954fa6a51768fc21c61
Deivid Urrego <coordinacion.tics@invercomes.com.co>
2020-11-23 10:13:56 -0500

    Primer Commit Se cargan los parametros iniciales del proyecto API REST TIENDA FULLGAME

---
+++ src/main/java/com/fullgame/ApiTiendafullgameApplication.java
@@ -0,0 +1,13 @@
+package com.fullgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTiendafullgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTiendafullgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullgame/ApiTiendafullgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTiendafullgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
